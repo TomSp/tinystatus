e8dc01b74fa0772eab11ba0f277b169995b8be2d
tom <tom@spengler.berlin>
2021-05-03 20:27:50 +0200

    use internal webserver

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd ${SCRIPT_DIR} && pwd)
 
 # Setup cron job
 stopservice crond
-echo "*/5 * * * * root cd $SCRIPT_DIR; ./tinystatus > ${SCRIPT_DIR}/../www/status.html" > /tmp/cron.d/tinystatus_cron
+echo "*/5 * * * * root cd $SCRIPT_DIR; ./tinystatus > /tmp/www/status.html" > /tmp/cron.d/tinystatus_cron
 startservice crond
 echo "Cron Job Added Successfully TinyStatus is now running in the background."
 
