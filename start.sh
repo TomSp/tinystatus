4e456c4889eaa6a7715aca87516c738261baa442
tom.spengler <tom.spengler@prisma-team.de>
2021-05-06 14:13:14 +0200

    fix run only once

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd ${SCRIPT_DIR} && pwd)
 
 # Setup cron job
 stopservice crond
-echo "*/3 * * * * root cd $SCRIPT_DIR; killall iperf3; ./tinystatus > /tmp/www/status.new && mv /tmp/www/status.new /tmp/www/status.html" > /tmp/cron.d/tinystatus_cron
+echo "*/3 2-23 * * * root cd $SCRIPT_DIR; killall iperf3; ./tinystatus > /tmp/www/status.new && mv /tmp/www/status.new /tmp/www/status.html" > /tmp/cron.d/tinystatus_cron
 startservice crond
 echo "Cron Job Added Successfully TinyStatus is now running in the background."
 
